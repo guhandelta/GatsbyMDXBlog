234f409e8c0d397b839e6e84308dec516db5973f
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-19 05:48:35 -0500

    Created a React component to count the likes and added it to the post.mdx

---
+++ src/components/counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+ const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="counter">
+            <h3>likes {count}</h3>
+            <button 
+                className="btn btn-success" 
+                onClick={() => setCount(count+1)}
+            >
+                Like
+            </button>
+        </div>
+    )
+}
+export default Counter
